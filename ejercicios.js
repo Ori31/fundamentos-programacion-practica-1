42bbf4f936a3e8b9e15f2334d349c9f163745737
Ori <shpersonal000@gmail.com>
2025-11-11 21:47:57 -0600

    Ejercicio 4.4

--- ejercicios.js
+++ ejercicios.js
@@ -251,7 +251,12 @@ function encontrarMaximo(numeros) {
  */
 function filtrarMayores(numeros, limite) {
   // TODO: Crea un nuevo array con solo los nÃºmeros mayores al lÃ­mite
-  const mayores = [];
+   const mayores = [];
+  for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] > limite) {
+      mayores.push(numeros[i]);
+    }
+  }
   return mayores;
 }
 
