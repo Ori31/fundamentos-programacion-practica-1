6a1caff76263e57de6c10c77aae0482fd66614c5
Ori <shpersonal000@gmail.com>
2025-11-11 21:43:46 -0600

    Ejercicio 3.1

--- ejercicios.js
+++ ejercicios.js
@@ -139,7 +139,12 @@ function clasificarEdad(edad) {
  */
 function factorial(n) {
   // TODO: Implementa usando un bucle for
-  return 1;
+   if (n === 0 || n === 1) return 1;
+  let resultado = 1;
+  for (let i = 2; i <= n; i++) {
+    resultado *= i;
+  }
+  return resultado;
 }
 
 /**
